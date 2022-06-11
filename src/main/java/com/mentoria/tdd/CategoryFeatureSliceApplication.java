8d1b7259391d94239e16634144c764fd0bc44a6d
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-06-11 20:56:23 -0300

    initial commit

---
+++ src/main/java/com/mentoria/tdd/CategoryFeatureSliceApplication.java
@@ -0,0 +1,13 @@
+package com.mentoria.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryFeatureSliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryFeatureSliceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentoria/tdd/CategoryFeatureSliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoria.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryFeatureSliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
