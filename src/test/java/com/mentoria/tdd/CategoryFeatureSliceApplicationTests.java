8798029ec4855b5317c93e76b4d65a7f0008d371
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-06-11 22:31:34 -0300

    new acceptance test definition

--- src/test/java/com/mentoria/tdd/CategoryFeatureSliceApplicationTests.java
+++ src/test/java/com/mentoria/tdd/CategoryFeatureSliceApplicationTests.java
@@ -1,14 +1,24 @@
 package com.mentoria.tdd;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 class CategoryFeatureSliceApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void should_return_200_when_getting_gategory_list() {
-		fail();
+	void should_return_200_when_getting_gategory_list() throws Exception {
+		mockMvc.perform(get("/api/categories").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 	}
 
 }
