1a6cc48e90bcd94d64bf22af4534dd4caa718c6f
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-06-12 14:23:42 -0300

    acceptance test definition

--- src/test/java/com/mentoria/tdd/CategoryFeatureSliceApplicationTests.java
+++ src/test/java/com/mentoria/tdd/CategoryFeatureSliceApplicationTests.java
@@ -6,9 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -17,9 +18,20 @@ class CategoryFeatureSliceApplicationTests {
 	@Autowired
 	MockMvc mockMvc;
 
-	@Test
-	void should_return_200_when_getting_gategory_list() throws Exception {
-		mockMvc.perform(get("/api/categories").contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
-	}
+    @Test
+    void should_return_200_when_getting_category_list() throws Exception {
+        mockMvc.perform(get("/api/categories").contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+    }
 
+    @Test
+    void should_return_category_list() throws Exception {
+        final var result = mockMvc.perform(get("/api/categories").contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(jsonPath("$", arrayWithSize(3)))
+                .andExpect(jsonPath("$[0]", hasSize(2)))
+                .andExpect(jsonPath("$[0]", hasProperty("codeInMarketplace")))
+                .andExpect(jsonPath("$[0].codeInMarketplace", isA(String.class)))
+                .andExpect(jsonPath("$[0]", hasProperty("name")))
+                .andExpect(jsonPath("$[0].name", isA(String.class)))
+                .andReturn();
+    }
 }
