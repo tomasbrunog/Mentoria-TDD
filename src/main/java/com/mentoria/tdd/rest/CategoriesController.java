87c6f14661633bc2a0fbb4285899c80b4bcd6d33
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-06-11 22:36:20 -0300

    green

---
+++ src/main/java/com/mentoria/tdd/rest/CategoriesController.java
@@ -0,0 +1,16 @@
+package com.mentoria.tdd.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoriesController {
+
+    @GetMapping("/api/categories")
+    @ResponseStatus(HttpStatus.OK)
+    public void getFirstLevelCategories() {
+
+    }
+}
