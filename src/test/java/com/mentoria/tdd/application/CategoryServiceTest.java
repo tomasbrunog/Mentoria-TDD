df4e93a2af029e4d14a251006d3cb01e780c81b2
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-06-13 23:57:03 -0300

    refactor test

--- src/test/java/com/mentoria/tdd/application/CategoryServiceTest.java
+++ src/test/java/com/mentoria/tdd/application/CategoryServiceTest.java
@@ -1,18 +1,15 @@
 package com.mentoria.tdd.application;
 
 import com.mentoria.tdd.domain.Category;
-import com.mentoria.tdd.domain.RemoteCategoryDto;
-import com.mentoria.tdd.domain.RemoteCategoryResponseDto;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CategoryServiceTest {
 
-    private final CategoryService service = new CategoryService(new RemoteCategoryService(new StubRemoteCategoryWebClient(buildResponseStubs())));
+    private final CategoryService service = new CategoryService(new RemoteCategoryService(StubRemoteCategoryWebClient.buildWith2PagesAnd3TotalRecords()));
 
     @Test
     void should_return_first_level_categories() {
@@ -30,16 +27,4 @@ class CategoryServiceTest {
 
         return List.of(categoryOne, categoryTwo, categoryThree);
     }
-
-    private List<RemoteCategoryResponseDto> buildResponseStubs() {
-        final var categoryOne = new RemoteCategoryDto(1, "Cat A");
-        final var categoryTwo = new RemoteCategoryDto(2, "Cat B");
-        final var categoryThree = new RemoteCategoryDto(3, "Cat C");
-
-        final var pageOne = new RemoteCategoryResponseDto(1, 3, List.of(categoryOne, categoryTwo));
-        final var pageTwo = new RemoteCategoryResponseDto(2, 3, Collections.singletonList(categoryThree));
-
-        return List.of(pageOne, pageTwo);
-    }
-
 }
