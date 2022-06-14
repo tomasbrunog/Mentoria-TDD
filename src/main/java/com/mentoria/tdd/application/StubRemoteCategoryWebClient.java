f66c3a5d90c906e4d29f9a3c7656f563cb045999
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-06-13 23:49:26 -0300

    refactor test

--- src/main/java/com/mentoria/tdd/application/StubRemoteCategoryWebClient.java
+++ src/main/java/com/mentoria/tdd/application/StubRemoteCategoryWebClient.java
@@ -1,7 +1,9 @@
 package com.mentoria.tdd.application;
 
+import com.mentoria.tdd.domain.RemoteCategoryDto;
 import com.mentoria.tdd.domain.RemoteCategoryResponseDto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class StubRemoteCategoryWebClient implements RemoteCategoryWebClient {
@@ -17,5 +19,14 @@ public class StubRemoteCategoryWebClient implements RemoteCategoryWebClient {
         return responseStubs.get(pageNumber - 1);
     }
 
+    public static StubRemoteCategoryWebClient buildWith2PagesAnd3TotalRecords() {
+        final var categoryOne = new RemoteCategoryDto(1, "Cat A");
+        final var categoryTwo = new RemoteCategoryDto(2, "Cat B");
+        final var categoryThree = new RemoteCategoryDto(3, "Cat C");
 
+        final var pageOne = new RemoteCategoryResponseDto(1, 3, List.of(categoryOne, categoryTwo));
+        final var pageTwo = new RemoteCategoryResponseDto(2, 3, Collections.singletonList(categoryThree));
+
+        return new StubRemoteCategoryWebClient(List.of(pageOne, pageTwo));
+    }
 }
--- src/test/java/com/mentoria/tdd/application/RemoteCategoryServiceTest.java
+++ src/test/java/com/mentoria/tdd/application/RemoteCategoryServiceTest.java
@@ -1,10 +1,8 @@
 package com.mentoria.tdd.application;
 
 import com.mentoria.tdd.domain.RemoteCategoryDto;
-import com.mentoria.tdd.domain.RemoteCategoryResponseDto;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -14,7 +12,7 @@ class RemoteCategoryServiceTest {
     @Test
     void should_fetch_first_level_categories_from_marketplace() {
         final var expected = buildWebClientExpectedResult();
-        final var stubWebClient = new StubRemoteCategoryWebClient(buildResponseStubs());
+        final var stubWebClient = StubRemoteCategoryWebClient.buildWith2PagesAnd3TotalRecords();
         final var service = new RemoteCategoryService(stubWebClient);
 
         final var result = service.getFirstLevelCategories();
@@ -29,16 +27,4 @@ class RemoteCategoryServiceTest {
                 new RemoteCategoryDto(3, "Cat C")
         );
     }
-
-    private List<RemoteCategoryResponseDto> buildResponseStubs() {
-        final var categoryOne = new RemoteCategoryDto(1, "Cat A");
-        final var categoryTwo = new RemoteCategoryDto(2, "Cat B");
-        final var categoryThree = new RemoteCategoryDto(3, "Cat C");
-
-        final var pageOne = new RemoteCategoryResponseDto(1, 3, List.of(categoryOne, categoryTwo));
-        final var pageTwo = new RemoteCategoryResponseDto(2, 3, Collections.singletonList(categoryThree));
-
-        return List.of(pageOne, pageTwo);
-    }
-
 }
