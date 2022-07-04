840e901a922f8822a2a73f0f78f8b8421cfc73f2
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-07-04 20:56:42 -0300

    green

--- src/main/java/com/mentoria/tdd/rest/CategoriesController.java
+++ src/main/java/com/mentoria/tdd/rest/CategoriesController.java
@@ -1,12 +1,23 @@
 package com.mentoria.tdd.rest;
 
+import com.mentoria.tdd.application.CategoryServiceAbstraction;
+import com.mentoria.tdd.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CategoriesController {
 
+    private final CategoryServiceAbstraction categoryService;
+
+    public CategoriesController(CategoryServiceAbstraction categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/api/categories")
-    public void getFirstLevelCategories() {
+    public List<Category> getFirstLevelCategories() {
+        return categoryService.getFirstLevelCategories();
     }
 }
