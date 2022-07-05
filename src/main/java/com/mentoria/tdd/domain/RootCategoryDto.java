6574b49decb78a670921314aefad96db5fe182ae
tomas.guagnini <tomas.guagnini@db1.com.br>
2022-07-05 00:43:49 -0300

    acceptance test - green

---
+++ src/main/java/com/mentoria/tdd/domain/RootCategoryDto.java
@@ -0,0 +1,11 @@
+package com.mentoria.tdd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RootCategoryDto {
+    private String codeInMarketplace;
+    private String name;
+}
--- src/main/java/com/mentoria/tdd/rest/CategoriesController.java
+++ src/main/java/com/mentoria/tdd/rest/CategoriesController.java
@@ -1,11 +1,12 @@
 package com.mentoria.tdd.rest;
 
 import com.mentoria.tdd.application.CategoryServiceAbstraction;
-import com.mentoria.tdd.domain.Category;
+import com.mentoria.tdd.domain.RootCategoryDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CategoriesController {
@@ -17,7 +18,7 @@ public class CategoriesController {
     }
 
     @GetMapping("/api/categories")
-    public List<Category> getFirstLevelCategories() {
-        return categoryService.getFirstLevelCategories();
+    public List<RootCategoryDto> getFirstLevelCategories() {
+        return categoryService.getFirstLevelCategories().stream().map(entity -> new RootCategoryDto(entity.getCodeInMarketplace(), entity.getName())).collect(Collectors.toList());
     }
 }
